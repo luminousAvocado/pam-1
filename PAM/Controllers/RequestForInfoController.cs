7618619ca95e6b6370669d57805f0a37406274e8
Lam <branlam@gmail.com>
2019-01-25 09:17:39 -0800

    non functional progress bar + prev button for some forms

--- PAM/Controllers/RequestForInfoController.cs
+++ PAM/Controllers/RequestForInfoController.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PAM.Models;
 using PAM.Data;
 using PAM.Extensions;
-using PAM.Models;
 
 namespace PAM.Controllers
 {
-    public class RequestTypeController : Controller
+    public class RequestForInfoController : Controller
     {
         private readonly UserService _userService;
 
-        public RequestTypeController(UserService userService)
+        public RequestForInfoController(UserService userService)
         {
             _userService = userService;
         }
-
+        
         [HttpGet]
-        public IActionResult RequestType(Requester requester)
+        public IActionResult RequestForInfo(Requester requester)
         {
-            var update = HttpContext.Session.GetObject<Request>("Request");
-            
-            return View("../Request/RequestType");
-        }
+            var newRequester = _userService.SaveRequester(requester);
 
+            return View("../Request/RequesterInfo");
+        }
     }
 }
--- PAM/Controllers/RequesterInfoController.cs
+++ PAM/Controllers/RequesterInfoController.cs
@@ -4,25 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PAM.Models;
-using PAM.Extensions;
 
 namespace PAM.Controllers
 {
     public class RequesterInfoController : Controller
     {
-        [HttpPost]
-        public IActionResult RequestSelf(Request req)
+        [HttpGet]
+        public IActionResult RequesterInfo(Requester requester)
         {
-            HttpContext.Session.SetObject("Request", req);
-            return RedirectToAction("RequestType", "RequestType", req);
-        }
-
-        [HttpPost]
-        public IActionResult RequestOther(Request req)
-        {
-            HttpContext.Session.SetObject("Request", req);
-            Requester newRequester = new Requester();
-            return View("../Request/RequesterInfo", newRequester);
+            return View("../Request/RequesterInfo");
         }
     }
 }
