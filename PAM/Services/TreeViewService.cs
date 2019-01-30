a520a3ebb1f73ed2cb1612470b9f88b56a1d1dbb
luminousAvocado <jamesckang@gmail.com>
2019-01-29 23:40:07 -0800

    Replacing old way with suns way. Initial creation of TreeViewService

---
+++ PAM/Services/TreeViewService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PAM.Data;
+
+namespace PAM.Services
+{
+    public class TreeViewService
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TreeViewService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+
+    public class TreeViewNode
+    {
+        public int Id { get; set; }
+        public int Type { get; set; }
+        public string Text { get; set; }
+        public List<TreeViewNode> Children { get; set; }
+        // Additional treeview properties
+    }
+
+
+}
