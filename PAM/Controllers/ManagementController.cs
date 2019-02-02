09277e73d5ad8786abe8106adb86a11870b1e82e
jborunda <jaimeborunda15@gmail.com>
2019-02-01 17:33:40 -0800

    managment for Systems and Locations implemented done

--- PAM/Controllers/ManagementController.cs
+++ PAM/Controllers/ManagementController.cs
@@ -25,14 +25,14 @@ namespace PAM.Controllers
             return View();
         }
         //-----------------------------------------------------------------------------------------------------------
+        //Burea CRUD operations
         // GET: Bureaus
         public async Task<IActionResult> Bureaus()
         {
             var appDbContext = _context.Bureaus.Include(b => b.BureauType);
             return View(await appDbContext.ToListAsync());
         }
-        //Burea CRUD operations 
-        //----------------------------------------------------------------------------------------------
+        
         // GET: Bureaus/Details/5
         public async Task<IActionResult> DetailsBureau(int? id)
         {
@@ -90,6 +90,7 @@ namespace PAM.Controllers
                 return NotFound();
             }
             ViewData["BureauTypeId"] = new SelectList(_context.Set<BureauType>(), "BureauTypeId", "Code", bureau.BureauTypeId);
+            
             return View(bureau);
         }
 
@@ -163,12 +164,6 @@ namespace PAM.Controllers
         {
             return _context.Bureaus.Any(e => e.BureauId == id);
         }
-
-
-
-
-
-
 
 
         //Units CRUD operations
@@ -322,51 +317,268 @@ namespace PAM.Controllers
         //Systems CRUD Actions
 
 
+        // GET: Systems1
         public async Task<IActionResult> Systems()
         {
-            var appDbContext = _context.Systems;
-            return View(await appDbContext.ToListAsync());
+            return View(await _context.Systems.ToListAsync());
         }
-        
-        public IActionResult DetailsSystem(int? id)
-        {
-            if (id == null)
-            { return NotFound(); }
-            //the obj matched to the ID
-            var System = _context.Systems.FirstOrDefault(s => s.SystemId == id);
-            if (System == null)
-            {
-                return NotFound();
-            }
-            return View(System);
-        }
-        
-        public IActionResult EditSystem()
-        {
 
-
-            return View();
-        }
-        
-        public IActionResult DeleteSystem(int? id)
+        // GET: Systems1/Details/5
+        public async Task<IActionResult> DetailsSystem(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-
-            var system = _context.Systems.FirstOrDefaultAsync(s => s.SystemId == id);
+            var system = await _context.Systems
+                .FirstOrDefaultAsync(m => m.SystemId == id);
             if (system == null)
             {
-                //system does not exist 
                 return NotFound();
             }
 
             return View(system);
         }
 
+        // GET: Systems1/Create
+        public IActionResult CreateSystem()
+        {
+            return View();
+        }
 
+        // POST: Systems1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateSystem([Bind("SystemId,Name,Description,Owner,Retired")] Models.System system)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(system);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Systems));
+            }
+            return View(system);
+        }
 
+        // GET: Systems1/Edit/5
+        public async Task<IActionResult> EditSystem(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var system = await _context.Systems.FindAsync(id);
+            if (system == null)
+            {
+                return NotFound();
+            }
+            return View(system);
+        }
+
+        // POST: Systems1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditSystem(int id, [Bind("SystemId,Name,Description,Owner,Retired")] Models.System system)
+        {
+            if (id != system.SystemId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(system);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SystemExists(system.SystemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Systems));
+            }
+            return View(system);
+        }
+
+        // GET: Systems1/Delete/5
+        public async Task<IActionResult> DeleteSystem(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var system = await _context.Systems
+                .FirstOrDefaultAsync(m => m.SystemId == id);
+            if (system == null)
+            {
+                return NotFound();
+            }
+
+            return View(system);
+        }
+
+        // POST: Systems1/Delete/5
+        [HttpPost, ActionName("DeleteSystem")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmedSystem(int id)
+        {
+            var system = await _context.Systems.FindAsync(id);
+            _context.Systems.Remove(system);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Systems));
+        }
+
+        private bool SystemExists(int id)
+        {
+            return _context.Systems.Any(e => e.SystemId == id);
+        }
+
+        //============================================================================
+        //Locations CRUD Actions
+        // GET: Locations
+        public async Task<IActionResult> Locations()
+        {
+            return View(await _context.Locations.ToListAsync());
+        }
+
+        // GET: Locations/Details/5
+        public async Task<IActionResult> DetailsLocation(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.LocationId == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // GET: Locations/Create
+        public IActionResult CreateLocation()
+        {
+            return View();
+        }
+
+        // POST: Locations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateLocation([Bind("LocationId,Name,Address,City,State,Zip")] Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(location);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Locations));
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Edit/5
+        public async Task<IActionResult> EditLocation(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations.FindAsync(id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+            return View(location);
+        }
+
+        // POST: Locations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditLocation(int id, [Bind("LocationId,Name,Address,City,State,Zip")] Location location)
+        {
+            if (id != location.LocationId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(location);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LocationExists(location.LocationId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Locations));
+            }
+            return View(location);
+        }
+
+        // GET: Locations/Delete/5
+        public async Task<IActionResult> DeleteLocation(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var location = await _context.Locations
+                .FirstOrDefaultAsync(m => m.LocationId == id);
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return View(location);
+        }
+
+        // POST: Locations/Delete/5
+        [HttpPost, ActionName("DeleteLocation")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmedLocation(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            _context.Locations.Remove(location);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Locations));
+        }
+
+        private bool LocationExists(int id)
+        {
+            return _context.Locations.Any(e => e.LocationId == id);
+        }
     }
 }
---
+++ PAM/Controllers/Systems1Controller.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PAM.Data;
+using PAM.Models;
+
+namespace PAM.Controllers
+{
+    public class Systems1Controller : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public Systems1Controller(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Systems1
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Systems.ToListAsync());
+        }
+
+        // GET: Systems1/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var system = await _context.Systems
+                .FirstOrDefaultAsync(m => m.SystemId == id);
+            if (system == null)
+            {
+                return NotFound();
+            }
+
+            return View(system);
+        }
+
+        // GET: Systems1/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Systems1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SystemId,Name,Description,Owner,Retired")] Models.System system)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(system);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(system);
+        }
+
+        // GET: Systems1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var system = await _context.Systems.FindAsync(id);
+            if (system == null)
+            {
+                return NotFound();
+            }
+            return View(system);
+        }
+
+        // POST: Systems1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SystemId,Name,Description,Owner,Retired")] Models.System system)
+        {
+            if (id != system.SystemId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(system);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SystemExists(system.SystemId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(system);
+        }
+
+        // GET: Systems1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var system = await _context.Systems
+                .FirstOrDefaultAsync(m => m.SystemId == id);
+            if (system == null)
+            {
+                return NotFound();
+            }
+
+            return View(system);
+        }
+
+        // POST: Systems1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var system = await _context.Systems.FindAsync(id);
+            _context.Systems.Remove(system);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SystemExists(int id)
+        {
+            return _context.Systems.Any(e => e.SystemId == id);
+        }
+    }
+}
