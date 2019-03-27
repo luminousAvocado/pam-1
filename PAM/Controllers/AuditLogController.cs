07c423a499527f6996ce111b51ea8b3d66e6a35d
luminousAvocado <jamesckang@gmail.com>
2019-03-26 18:48:48 -0700

    Initial creation of auditlogs controller and routing of Reports button

---
+++ PAM/Controllers/AuditLogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PAM.Controllers
+{
+    public class AuditLogController : Controller
+    {
+        public AuditLogController()
+        {
+
+        }
+
+        public IActionResult AuditLogs()
+        {
+            return View();
+        }
+    }
+}
