f7e8f67cc82d1132843f4d8aa2eba37a37f1dbb9
luminousAvocado <jamesckang@gmail.com>
2019-03-28 14:27:14 -0700

    Implemented initial view for AuditLogs, functionality for listing all logs and sorting by columns

--- PAM/Controllers/AuditLogController.cs
+++ PAM/Controllers/AuditLogController.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PAM.Data;
 
 namespace PAM.Controllers
 {
     public class AuditLogController : Controller
     {
-        public AuditLogController()
-        {
+        private readonly AuditLogService _auditService;
+        private readonly ILogger<AuditLogController> _logger;
 
+        public AuditLogController(AuditLogService auditService, ILogger<AuditLogController> logger)
+        {
+            _auditService = auditService;
+            _logger = logger;
         }
 
         public IActionResult ListLogs()
         {
-            return View();
+            return View(_auditService.GetAllLogs());
         }
     }
 }
