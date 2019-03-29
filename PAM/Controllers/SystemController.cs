60dc22687419db0954adba2e3b36c2f07da50b48
luminousAvocado <jamesckang@gmail.com>
2019-03-28 21:27:46 -0700

    Implemented system auditLogging

--- PAM/Controllers/SystemController.cs
+++ PAM/Controllers/SystemController.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using PAM.Data;
+using PAM.Extensions;
+using Newtonsoft.Json;
+using System;
+using System.Security.Claims;
+using PAM.Models;
 
 namespace PAM.Controllers
 {
@@ -12,13 +17,15 @@ namespace PAM.Controllers
         private readonly OrganizationService _organizationService;
         private readonly IMapper _mapper;
         private readonly ILogger<SystemController> _logger;
+        private readonly AuditLogService _auditService;
 
-        public SystemController(SystemService systemService, OrganizationService organizationService, IMapper mapper, ILogger<SystemController> logger)
+        public SystemController(SystemService systemService, OrganizationService organizationService, IMapper mapper, ILogger<SystemController> logger, AuditLogService auditService)
         {
             _systemService = systemService;
             _organizationService = organizationService;
             _mapper = mapper;
             _logger = logger;
+            _auditService = auditService;
         }
 
 
@@ -43,6 +50,9 @@ namespace PAM.Controllers
         public IActionResult AddSystem(Models.System system)
         {
             system = _systemService.AddSystem(system);
+
+            _auditService.CreateAuditLog(Int32.Parse(((ClaimsIdentity)User.Identity).GetClaim("EmployeeId")), Models.Action.Create, ResourceType.System, system.SystemId);
+
             return RedirectToAction(nameof(ViewSystem), new { id = system.SystemId });
         }
 
@@ -57,8 +67,14 @@ namespace PAM.Controllers
         public IActionResult EditSystem(int id, Models.System update)
         {
             var system = _systemService.GetSystem(id);
+            var oldValue = JsonConvert.SerializeObject(system);
+
             _mapper.Map(update, system);
             _systemService.SaveChanges();
+            var newValue = JsonConvert.SerializeObject(_systemService.GetSystem(id));
+
+            _auditService.CreateAuditLog(Int32.Parse(((ClaimsIdentity)User.Identity).GetClaim("EmployeeId")), Models.Action.Edit, ResourceType.System, id, oldValue, newValue);
+
             return RedirectToAction(nameof(ViewSystem), new { id });
         }
 
@@ -67,6 +83,9 @@ namespace PAM.Controllers
             var system = _systemService.GetSystem(id);
             system.Retired = true;
             _systemService.SaveChanges();
+
+            _auditService.CreateAuditLog(Int32.Parse(((ClaimsIdentity)User.Identity).GetClaim("EmployeeId")), Models.Action.Delete, ResourceType.System, id);
+
             return RedirectToAction(nameof(Systems));
         }
     }
